d8c325ba3d969313446a017c46ca24c9dfe2a9e7
MarkPT888 <mtopholt@gmail.com>
2026-04-09 15:39:25 +0200

    Started working on user login validation

---
+++ backend/src/main/java/dk/StudyBobby/backend/dto/userRequests/LoginRequest.java
@@ -0,0 +1,23 @@
+package dk.StudyBobby.backend.dto.userRequests;
+
+public class LoginRequest {
+    private String name;
+    private String password;
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- backend/src/main/java/dk/StudyBobby/backend/services/UserService.java
+++ backend/src/main/java/dk/StudyBobby/backend/services/UserService.java
@@ -1,10 +1,12 @@
 package dk.StudyBobby.backend.services;
 
+import dk.StudyBobby.backend.dto.userRequests.LoginRequest;
 import dk.StudyBobby.backend.dto.userRequests.UserCreateRequest;
 import dk.StudyBobby.backend.entities.User;
 import dk.StudyBobby.backend.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 @Service
@@ -39,6 +41,17 @@ public class UserService {
     }
 
     // VALIDATE login credentials
+    public Long validate(LoginRequest request) {
+        User user = userRepo.findByName(request.getName())
+                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+
+        return user.getId(); // we have to return the ID to frontend to give own session
+    }
+
+
+
+
+
     // Get the name from their entry first
     // Get the list of names from name column in User table in DB
     // Compare the name entered to all names in name column of User table in DB
@@ -47,4 +60,7 @@ public class UserService {
     // If name matches, check if entered password matches corresponding password in
     // the row in which the user's name matched
     // Return HTTP code
+
+
+
 }
